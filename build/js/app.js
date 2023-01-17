0a026bd148bb17330b15c8dee18c69083a2b6f44
Marco Hernandez <marco7245@hotmail.com>
2023-01-17 16:37:06 -0600

    Index Terminado

--- build/js/app.js
+++ build/js/app.js
@@ -3,7 +3,41 @@ document.addEventListener('DOMContentLoaded',function(){
 });
 
 function iniciarApp(){
+    navegacionFija();
     crearGaleria();
+    scrollNav();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const nosotros = document.querySelector('.sobre-nosotros');
+    const body = document.querySelector('body');
+
+
+    window.addEventListener('scroll', function() {
+        if( nosotros.getBoundingClientRect().bottom < 0  ) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    });
+}
+
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach( enlace =>{
+        enlace.addEventListener('click',function(e){
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+
+            seccion.scrollIntoView({behavior:"smooth"});
+        });
+    });
 }
 
 function crearGaleria(){
--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,30 @@ const {src, dest, watch, parallel} = require("gulp");
 // CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes
 
 const cache = require('gulp-cache')
-const imagemin = require('gulp-imagemin');
+const imagemin =require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
+
+//JavaScript
+
+const terser = require('gulp-terser-js');
 
 function css(done){
 
     src('src/scss/**/*.scss')       //Identificar SASS
+    .pipe( sourcemaps.init() )
     .pipe( plumber() )
     .pipe( sass() )                //Compilar SASS
+    .pipe( postcss ([autoprefixer(), cssnano() ]) )
+    .pipe( sourcemaps.write('.') )
     .pipe( dest("build/css") );    //Almacenar SASS
 
     done();
@@ -63,6 +74,9 @@ function versionAvif(done){
 function javascript(done){
     
     src('src/js/**/*.js')
+    .pipe( sourcemaps.init() )
+    .pipe( terser() )
+    .pipe( sourcemaps.write('.') )
     .pipe( dest('build/js') );
 
     done();
