171e1150f7f7774a7099157d208297f771e8b9ae
Marco Hernandez <marco7245@hotmail.com>
2023-01-16 18:27:34 -0600

    Section Terminada

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, parallel} = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Imagenes
+
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
 
@@ -12,6 +21,45 @@ function css(done){
     done();
 }
 
+function imagenes(done){
+
+    const opciones = {
+        optimizationLevel : 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe( cache( imagemin(opciones) ) )
+    .pipe( dest('build/img') )
+
+    done();
+}
+
+function versionWebp(done){
+
+    const opciones = {
+        quality: 100
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe( webp(opciones) )
+    .pipe( dest('build/img') );
+
+    done();
+}
+
+function versionAvif(done){
+
+    const opciones = {
+        quality: 100
+    };
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe( avif(opciones) )
+    .pipe( dest('build/img') );
+
+    done();
+}
+
 function dev( done ) {
     watch('src/scss/**/*.scss', css);
     //watch('src/js/**/*.js', javascript);
@@ -19,4 +67,7 @@ function dev( done ) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes,versionWebp,versionAvif,dev);
