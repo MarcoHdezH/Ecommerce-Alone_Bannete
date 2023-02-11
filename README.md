2a099f87e4d22881286e1b3438e1d6eaacbfaae2
Marco Hernandez <marco7245@hotmail.com>
2023-02-11 17:54:37 -0600

    Semana 10 Añadida

--- README.md
+++ README.md
@@ -21,4 +21,6 @@ El .js de la semana 8 se llama "Orden-Superior.js"
 
 La semana 9 se encuentra en la carpeta DOM
 
+La semana 10 se encuentra en la carpeta Semana 10 (Para cargar la API debe comentar el Script 'json.js' y viceversa)
+
  *Solo hice el paso 3 y 4, el local storage esta funcionando en el index.html* 
---
+++ Semana 10/json.js
@@ -0,0 +1,34 @@
+const cargarJSONArrayBtn = document.querySelector('#cargarJSON');
+cargarJSONArrayBtn.addEventListener('click', obtenerDatos);
+
+
+function obtenerDatos() {
+    fetch('empleados.json') 
+        .then( respuesta => {
+            return respuesta.json()
+        }) 
+        .then(resultado => {
+            mostrarHTML(resultado);
+            console.log(resultado)
+        })
+}
+
+function mostrarHTML(empleados) {
+    const contenido = document.querySelector('#contenido');
+
+    let html = '';
+
+    empleados.forEach( empleado => {
+        const { id, nombre, empresa, trabajo} = empleado;
+
+        html += `
+            <p>ID: ${id} </p>
+            <p>Empleado: ${nombre} </p>
+            <p>Empresa: ${empresa} </p>
+            <p>Trabajo: ${trabajo} </p>
+        `
+    });
+
+    contenido.innerHTML = html;
+    
+}
